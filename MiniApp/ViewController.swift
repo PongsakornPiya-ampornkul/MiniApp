0fc1dd2eeca4737641ac1068ab8841e328275f6b
Pongsakorn Piya-ampornkul <88673389+PongsakornPiya-ampornkul@users.noreply.github.com>
2021-10-09 20:16:39 +0700

    Initial Commit

---
+++ MiniApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniApp
+//
+//  Created by Pongsakorn Piya-ampornkul on 9/10/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
