82194a741b2d79cbf7554622ca941c645621c6c3
Pongsakorn Piya-ampornkul <88673389+PongsakornPiya-ampornkul@users.noreply.github.com>
2021-10-09 21:04:21 +0700

    Add Constant File

---
+++ MiniApp/Constants/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  MiniApp
+//
+//  Created by Pongsakorn Piya-ampornkul on 9/10/2564 BE.
+//
+
+import UIKit
+import Foundation
+
+//name
+let MINI_APP_NAME = "MiniApp"
+
+//Json_Code
+let JSON_CODE = "code"
+let JSON_SUCCESS = "success"
+let JSON_ERROR = "error"
+let JSON_DATA = "data"
